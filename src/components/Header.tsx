8dc46460c6f239808d8802ad4caa50e21d6d1a3b
Skip <skip@usecurling.com>
2026-03-26 11:22:36 +0000

    Auto-commit after full sync (build simulation)

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,7 +11,7 @@ export const Header = () => {
           className="flex items-center transition-transform hover:scale-105 active:scale-95"
         >
           <img
-            src="./public/logo.jpg"
+            src="/logo.jpg"
             //            alt="Logo SACRE - Soluções Integradas de Água para Cidades Resilientes"
             className="h-10 sm:h-12 w-auto drop-shadow-sm"
           />
