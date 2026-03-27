57622cfc73d5bd712ec2ac19197d00a22bf0239f
Skip <skip@usecurling.com>
2026-03-27 12:45:07 +0000

    Auto-commit after full sync (build simulation)

--- src/pages/institucional/Projeto.tsx
+++ src/pages/institucional/Projeto.tsx
@@ -69,6 +69,28 @@ export default function Projeto() {
           </CardContent>
         </Card>
       </div>
+        <div className="w-20 h-1.5 bg-secondary mb-6 rounded-full" />
+        <p className="text-lg text-muted-foreground leading-relaxed text-justify mb-6">
+          O desenvolvimento de métodos de monitoramento e de remediação de aquíferos contaminados por fontes multipontuais e dispersas, com base em NE&MS, proporcionará avanços científicos inéditos ao país e poderá gerar tecnologias patenteáveis. Todas as soluções de aumento de oferta, tratamento e alocação de águas rurais e urbanas serão avaliadas dentro de um novo arranjo administrativo de gestão de recurso hídrico, que levará em conta aspectos socioeconômicos, hábitos sociais dos usuários (“produtores públicos e privados de água⁶”) e as políticas de uso da terra. As soluções técnicas e de envolvimento institucional serão testadas de forma pioneira em um estudo nas cabeceiras das bacias hidrográficas dos rios Bauru-Batalha, onde se localizam parte dos municípios de Bauru, Piratininga e Agudos (SP).
+        </p>
+
+        <p className="text-lg text-muted-foreground leading-relaxed text-justify mb-6">
+         Toda a experiência científica interdisciplinar de ponta nas áreas de hidrogeologia, engenharia hidrológica, socioeconomia e governança hídrica, baseada no desenvolvimento e adaptação de NE&MS, permitirá criar uma plataforma digital hidroeconômica de múltiplas soluções (HYMP: HYdroeconomic, Multiple solutions Platform) que auxiliará na gestão dos recursos hídricos adequada às condições biofísicas e socioeconômicas do Estado de São Paulo. O Projeto SACRE traz também uma outra novidade, que é o envolvimento das principais lideranças institucionais na gestão dos recursos hídricos subterrâneos do Estado de São Paulo (IPA, CETESB, IPT, SIMAL, DAEE), consorciadas a universidades nacionais (USP, UNESP, UNICAMP e UNIFESP) e estrangeiras (University of Waterloo do Canadá e Hiroshima University do Japão), recebendo apoio formal dos governos do Estado de São Paulo, do Canadá e do Município de Bauru. No total são 41 profissionais, a maioria com título superior a doutor.
+        </p>
+
+        <p className="text-lg text-muted-foreground leading-relaxed text-justify">
+          A formação de recursos humanos é outro ponto de destaque do SACRE. Hoje o projeto conta com 26 alunos e pesquisadores bolsistas e outros 4 não bolsistas, incluindo 1 Jovem Pesquisador; 2 Pós-Doutorados (PD); 5 Doutorados Direto; 3 Doutorados; 5 Mestrados; 4 Treinamento Tecnológicos (TT); 4 Iniciação Científica (IC); e 4 Trabalho de Conclusão de Curso (e ainda 8 bolsas não ocupadas, incluindo 1 PD, 1 Jornalismo Científico, 3 IC e 3 TT). Cursos e treinamento in loco também são desenvolvidos e voltados tanto para a os stakeholders locais e estaduais, como de Comitês de Bacia e de organismos nacionais.
+        </p>
+
+        <p className="text-lg text-muted-foreground leading-relaxed text-justify">
+          A comunicação científica tem papel decisivo no SACRE, pois entende-se que políticas públicas são efetivas se construídas em acordo com a sociedade e os seus stakeholders. Como um projeto científico, o letramento científico⁷ é fundamental para a sociedade contemporânea, sobretudo em temas novos, como as mudanças climáticas globais e soluções baseadas na natureza. Por essa razão, o projeto conta em sua equipe com jornalistas e comunicadores científicos, além de realizar eventos próprios, como o Encontro das Águas⁸, HidroGeoDia⁹, e produtos específicos em mídia digital.
+        </p>
+
+        <p className="text-lg text-muted-foreground leading-relaxed text-justify">
+          Ao final do Projeto SACRE, um fórum permanente será criado com o propósito de apoiar a gestão das águas subterrâneas, através das agências governamentais parceiras. O SACRE conta com o financiamento da FAPESP pelo Projeto Temático (2020/15434-0) e de Jovem Pesquisador (2021/12817-8); do CNPq pelo Projeto Universal (423950/2021-5); e de bolsas de estudos da FAPESP, CAPES, CNPq e da própria USP, superando o montante de 11 milhões de reais.
+        </p>
+      </div>
+
     </div>
   )
 }
