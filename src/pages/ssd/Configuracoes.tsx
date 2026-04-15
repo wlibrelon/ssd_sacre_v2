f9316d08aea7dfd1365a0eeed32c64d8e3385e94
Skip <skip@usecurling.com>
2026-04-15 16:30:27 +0000

    Auto-commit after full sync (build simulation)

--- src/pages/ssd/Configuracoes.tsx
+++ src/pages/ssd/Configuracoes.tsx
@@ -37,6 +37,7 @@ const Configuracoes: React.FC = () => {
     'Condição atual de captação',
     'Expansão de poços no município',
     'Mais poços na área urbana',
+    'Captações a Montante',
   ]
 
   const addCenario = () => {
