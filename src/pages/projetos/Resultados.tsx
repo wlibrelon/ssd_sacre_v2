c62cf65c1c57d1ea2600abe19c5f4d9655af9330
Skip <skip@usecurling.com>
2026-06-18 13:22:08 -0300

    Auto-commit after full sync (build simulation)

--- src/pages/projetos/Resultados.tsx
+++ src/pages/projetos/Resultados.tsx
@@ -131,7 +131,7 @@ export default function Resultados() {
               <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-[calc(100vw-2rem)] sm:w-[400px] p-0">
+          <PopoverContent className="w-full p-0">
             <Command>
               <CommandInput placeholder="Buscar WP..." />
               <CommandList>
